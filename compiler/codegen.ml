3765e1ef08dc2aba11740933fccae911ad4ee510
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2009-04-28 22:35:21 -0700

    Worked on codegen of struct repn using objects.
    Signed-off-by: Prashanth Mundkur <prashanth.mundkur@gmail.com>

--- compiler/codegen.ml
+++ compiler/codegen.ml
@@ -58,6 +58,20 @@ module Typeinfo = struct
 
     | _ -> assert false
 
+  let class_arg_type fid ft =
+    match ft with
+      | Ttype_base bt ->
+          (lib_module_name bt) ^ ".t"
+      | Ttype_struct _ ->
+          String.capitalize (Ident.name_of fid) ^ ".o"
+      | Ttype_array _  ->
+          String.capitalize (Ident.name_of fid) ^ ".o array"
+      | Ttype_map _ ->
+          String.capitalize (Ident.name_of fid) ^ ".t"
+      | Ttype_label ->
+          ""
+      | Ttype_format fname ->
+          String.capitalize (Location.node_of fname) ^ ".o"
 end
 
 module Variant = struct
@@ -121,48 +135,106 @@ module Variant = struct
     fprintf ff "@]@]@,"
 
   let generate_defs ff id ft v =
-    gen_type ff id ft v; fprintf ff "@,";
-    gen_of ff id ft v;   fprintf ff "@,";
-    gen_to ff id ft v;   fprintf ff "@,";
-    gen_to_string ff id ft v
+    gen_type ff id ft v;        fprintf ff "@,";
+    gen_of ff id ft v;          fprintf ff "@,";
+    gen_to ff id ft v;          fprintf ff "@,";
+    gen_to_string ff id ft v;   fprintf ff "@,"
 
 end
 
+module Object = struct
+  let arg_name id = Ident.name_of id ^ "_arg"
+
+  let args_of st =
+    Ident.fold (fun id (ft, _) o_args ->
+                  let at = Typeinfo.class_arg_type id ft in
+                    if at <> "" then (id, at) :: o_args else o_args
+               ) st.fields []
+
+  let field_method ff id ft fa =
+    let getter = "get_" ^ Ident.name_of id in
+    let c_arg = arg_name id in
+      match ft, fa.field_attrib_variant with
+        | Ttype_base ((Tbase_primitive _) as bt), None ->
+            fprintf ff "@,method %s = %s.read %s"
+              getter (Typeinfo.lib_module_name bt) c_arg
+        | Ttype_base ((Tbase_vector _) as bt), None ->
+            fprintf ff "@,method %s_at indx = %s.read_elem indx %s"
+              getter (Typeinfo.lib_module_name bt) c_arg
+        | Ttype_base bt, Some _ ->
+            fprintf ff "@,method %s = %s (%s.read %s)"
+              getter (Variant.of_name id) (Typeinfo.lib_module_name bt) c_arg
+        | Ttype_struct _, _ | Ttype_format _, _ | Ttype_map _, _->
+            fprintf ff "@,method %s = %s" getter c_arg
+        | Ttype_array _, _ ->
+            fprintf ff "@,method %s_at indx = %s.(indx)" getter c_arg
+        | Ttype_label, _ ->
+            ()
+
+  let generate ff st =
+    let args = List.map (fun (id, at) ->
+                           Printf.sprintf "(%s : %s)" (arg_name id) at
+                        ) (args_of st)
+    in
+      fprintf ff "@[<v 2>class o %s = object" (String.concat " " args);
+      Ident.iter (fun id (ft, fa) -> field_method ff id ft fa) st.fields;
+      fprintf ff "@]@,end@,"
+end
+
 module Struct = struct
-  let start_module ff ?(suffix="") st_id =
+  let module_name ?(suffix="") st_id =
     let suffix = if suffix = "" then "" else "_" ^ (String.lowercase suffix) in
-      fprintf ff "@[<v 2>module %s = struct@,"
-        (String.capitalize ((Ident.name_of st_id) ^ suffix))
+      String.capitalize (Ident.name_of st_id) ^ suffix
+
+  let start_module ff ?(suffix="") st_id =
+    fprintf ff "@[<v 2>module %s = struct@," (module_name st_id ~suffix)
 
   let end_module ff =
     fprintf ff "@]@,end@,"
 
-  let rec generate ff typed_env st_id ?(suffix="") (st, deps) =
-    start_module ff ~suffix st_id;
-    (* variants *)
+  let generate_variants ff (st, deps) =
     Ident.iter (fun id (ft, fattribs) ->
                   match fattribs.field_attrib_variant with
                     | None -> ()
                     | Some (v, _) -> Variant.generate_defs ff id ft v
-               ) st.fields;
+               ) st.fields
+
+  let rec generate_map ff typed_env id (bid, mt, deps) =
+    start_module ff id;
+    StringMap.iter (fun cnm (_, _, st) ->
+                      generate ff typed_env id ~suffix:cnm (st, deps)
+                   ) mt.map_type_desc;
+    fprintf ff "@[<v 2>type t =@,";
+    StringMap.iter (fun cnm _ ->
+                      fprintf ff "| %s of %s.o@,"
+                        (String.capitalize cnm) (module_name id ~suffix:cnm)
+                   ) mt.map_type_desc;
+    fprintf ff "@]@,";
+    end_module ff
+
+  and generate ff typed_env st_id ?(suffix="") (st, deps) =
+    start_module ff ~suffix st_id;
+    generate_variants ff (st, deps);
+
     (* nested structs *)
     Ident.iter (fun id (ft, fattribs) ->
                   match ft with
                     | Ttype_struct st
                     | Ttype_array (_, st) ->
                         generate ff typed_env id (st, deps)
-                    | Ttype_map (_, mt) ->
-                        StringMap.iter
-                          (fun cnm (_, _, st) ->
-                             generate ff typed_env id ~suffix:cnm (st, deps)
-                          ) mt.map_type_desc
+                    | Ttype_map (bid, mt) ->
+                        generate_map ff typed_env id (bid, mt, deps)
                     | _ ->
                         ()
                ) st.fields;
-    (* TODO: struct rep. This requires computing the free variables of
-       the struct, which need to be passed in as arguments.  Also,
-       some free variables are special: e.g. remaining(),
-       offset(field), etc. in array/vector lengths. *)
+
+    Object.generate ff st;
+
+    (* TODO: struct unmarshalling/marshalling. This requires computing
+       the free variables of the struct, which need to be passed in as
+       arguments.  Also, some free variables are special:
+       e.g. remaining(), offset(field), etc. in array/vector
+       lengths. *)
     end_module ff
 end
 
